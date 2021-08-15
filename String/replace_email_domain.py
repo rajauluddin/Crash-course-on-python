ec68405641531c1454ab30e5e6dc953fbc73a1a5
Rajaul Uddin <35023610+rajauluddin@users.noreply.github.com>
2021-08-15 23:31:55 +0545

    String
    worked exaple

---
+++ String/replace_email_domain.py
@@ -0,0 +1,10 @@
+
+from typing import overload
+
+
+def replace_domain(email, old_domain, new_domain):
+    if "@" + old_domain in email:
+        index = email.index("@"+ old_domain)
+        new_domain = email[:index] + "@" + new_domain
+        return new_domain
+    return email
