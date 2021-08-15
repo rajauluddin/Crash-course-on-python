d15603a887191fb96da14ccb60aeb93ad8d86a5c
Rajaul Uddin <35023610+rajauluddin@users.noreply.github.com>
2021-08-15 22:13:37 +0545

    Create double_word.py
    double the word and also find its length

---
+++ String/double_word.py
@@ -0,0 +1,6 @@
+def double_word(word):
+    return word + word + str(len(word*2))
+
+print(double_word("hello")) # Should return hellohello10
+print(double_word("abc"))   # Should return abcabc6
+print(double_word(""))      # Should return 0
