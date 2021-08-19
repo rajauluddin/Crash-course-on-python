eaf012e81ff1793e0cb36e78da03dab0b2a20d91
Rajaul Uddin <35023610+rajauluddin@users.noreply.github.com>
2021-08-20 01:36:37 +0545

    list and tuples

--- String/iterating_list_tuples.py
+++ String/iterating_list_tuples.py
@@ -6,6 +6,7 @@ for animal in animals:
 print("total characters: {}, average length: {}".format(chars,chars/len(animals)))
 print("---------------")
 
+#The enumerate() function takes a list as a parameter and returns a tuple for each element in the list. The first value of the tuple is the index and the second value is the element itself.
 winners = ["ashley", "dylan", "resse"]
 for index, person in enumerate(winners):
     print("{} - {}".format(index + 1,person))
